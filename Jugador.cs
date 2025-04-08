9474ed8beae73d7b5f9349d7fca8018b192da0da
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-04-07 22:03:32 -0500

    Clase Jugador y Puntaje creadas para controlar estado y puntaje acumulado

---
+++ Jugador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wTriviaPro
+{
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public Puntaje Puntaje { get; private set; }
+
+        public Jugador(string nombre)
+        {
+            Nombre = nombre;
+            Puntaje = new Puntaje();
+        }
+
+        public void ReiniciarPuntaje()
+        {
+            Puntaje = new Puntaje();
+        }
+    }
+}
