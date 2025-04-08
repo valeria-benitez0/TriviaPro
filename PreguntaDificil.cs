04be483189ed5d9abd16a135b5518b77d9567b51
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-04-07 22:00:10 -0500

    Clases derivadas de Pregunta implementadas con lógica de puntaje por dificultad

---
+++ PreguntaDificil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wTriviaPro
+{
+    public class PreguntaDificil : Pregunta
+    {
+        public PreguntaDificil(string texto, List<string> opciones, string respuestaCorrecta, string categoria)
+            : base(texto, opciones, respuestaCorrecta, categoria, "Difícil") { }
+
+        public override int EvaluarRespuesta(string respuestaUsuario)
+        {
+            return respuestaUsuario == RespuestaCorrecta ? 15 : -2;
+        }
+    }
+}
---
+++ PreguntaMedia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wTriviaPro
+{
+    public class PreguntaMedia : Pregunta
+    {
+        public PreguntaMedia(string texto, List<string> opciones, string respuestaCorrecta, string categoria)
+            : base(texto, opciones, respuestaCorrecta, categoria, "Medio") { }
+
+        public override int EvaluarRespuesta(string respuestaUsuario)
+        {
+            return respuestaUsuario == RespuestaCorrecta ? 10 : -2;
+        }
+    }
+}
